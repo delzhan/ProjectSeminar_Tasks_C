9671d1a6034b76bb9347158400f7070eaefb4524
Delzhan <polya.taraa@gmail.com>
2024-12-12 10:18:28 +0300

    Add files via upload

---
+++ less1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream> 
+#include <math.h> 
+#include <cstdlib> // для system 
+using namespace std;
+
+int main()
+{
+    const double EulerConstant = exp(1);
+    cout << round(EulerConstant * 100) / 100 << endl;
+    system("pause"); // Только для тех, у кого MS Visual Studio 
+    return 0;
+}
