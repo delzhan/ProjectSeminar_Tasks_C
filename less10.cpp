8fb0de46344686cc3203a3ba0b2fb319e44d648d
Delzhan <polya.taraa@gmail.com>
2025-01-23 19:03:38 +0300

    Add files via upload

---
+++ less10.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+//5.1) Напечатать ряд чисел 20 в виде: 20 20 20 20 20 20 20 20 20 20.
+{
+    setlocale(0, "rus");
+    int Count, Number;
+
+    cout << "Введите количество повторений заданного значения: ";
+    cin >> Count;
+    Number = 20;
+
+    for (int i = 0; i < Count; i++) {
+        cout << Number; // Вывод числа
+        if (i < Count - 1) {
+           cout << " "; 
+        }
+    }
+    return 0;
+}
