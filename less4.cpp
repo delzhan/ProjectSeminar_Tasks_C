41b79159b46750013140501241b57fe3e399dd1f
Delzhan <polya.taraa@gmail.com>
2025-02-17 12:38:09 +0300

    Update less4.cpp

--- less4.cpp
+++ less4.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 //2.24) Из трехзначного числа x вычли его последнюю цифру. Когда результат разделили на 10, а к частному слева приписали последнюю цифру числа x, то получилось число 237. Найти число x.
 int main()
@@ -8,8 +8,8 @@ int main()
 
 		int LastNumber = x % 10;
 		int Stub = x - LastNumber;
-		int Private = x / 10;
-		int Result = LastNumber * 100 + (Stub/10);
+		int Private = Stub/ 10;
+		int Result = LastNumber * 100 + Private;
 
 		if (Result == 237) {
 			std::cout << "Значение x, соответствующее условию задачи: "<< x << std::endl;
